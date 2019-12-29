82e0e753b0220b540c7cb1412db8d7755f28a426
RAMKUMAR-devops <ramkumar.pudi01@gmail.com>
2019-12-29 13:58:11 +0530

    commenting

--- print.sh
+++ print.sh
@@ -1,13 +1,24 @@
-#/!bin/bash
+#!/bin/bash 
 
-ACTION=$1
+# If you assign a name to set of commands then it is a function.
 
-if [ "$ACTION" == "start" ]; then 
-  echo "Starting Service"
-elif [ "$ACTION" == "stop" ]; then 
-  echo "Stopping Service"
-else  
-  echo "Invalid INput"
-  echo -e "Accepted Values are \e[31mstart/stop\e[0m"
-  exit 1
-fi 
+# Declare a function 
+function abc() {
+  echo I am a function 
+}
+
+## One more function 
+abc1() {
+  echo I am also a function
+  return 99
+  echo Bye
+}
+
+
+## Call function 
+abc
+abc1 
+echo Exit Status = $?
+## abc is a command because the first argument which we execute is a command. Function is a type of command out of four types.
+
+# Alike commands , function also  have exit status. Exit status of function can be given by using a return command. return is exactly same like exit command but for functions.
