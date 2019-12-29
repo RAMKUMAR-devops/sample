fd703a716d673cba452d16a826bae90650863d08
RAMKUMAR-devops <ramkumar.pudi01@gmail.com>
2019-12-29 13:56:12 +0530

    commenting

--- print.sh
+++ print.sh
@@ -1,17 +1,13 @@
-#!/bin/bash 
+#/!bin/bash
 
 ACTION=$1
 
-case $ACTION in 
-  start)
-    echo -e "Starting Service"
-    ;;
-  stop)
-    echo -e "Stopping Service"
-    ;;
-  *)
-    echo -e "Invalid Input"
-    echo -e "Expected inputs are \e[1mstart/stop\e[0m"
-    exit 1
-    ;;
-esac
+if [ "$ACTION" == "start" ]; then 
+  echo "Starting Service"
+elif [ "$ACTION" == "stop" ]; then 
+  echo "Stopping Service"
+else  
+  echo "Invalid INput"
+  echo -e "Accepted Values are \e[31mstart/stop\e[0m"
+  exit 1
+fi 
