37ae4a0f5597a700b8462037f47371e4251752c9
RAMKUMAR-devops <ramkumar.pudi01@gmail.com>
2019-12-29 11:35:34 +0530

    commenting

--- print.sh
+++ print.sh
@@ -1,3 +1,5 @@
- echo -e "helloo" \n
-echo -e "single line"\t
+ echo -e "helloo \n"
+echo -e "single line\t"
 echo -e "ram\tkumar\tpudi\nmalleswara\trao\tpudi"
+
+echo -e "\e[35m MAGENTA COLOR FONT[0m"
